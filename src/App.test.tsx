234ff6cbec1a55d722ad3ba5cffa2203af7f8fb3
deger4beger <deger.begerrr@gmail.com>
2022-01-19 15:02:56 +0600

    app test fixed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,18 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
-import * as Redux from "react-redux"
+import * as CustomReduxHooks from "./hooks/redux"
 import Header from './components/single/Header/Header';
 import Footer from './components/single/Footer/Footer';
 import AppRouter from './router/AppRouter';
 import App from "./App"
 import AppProvider from './providers/AppProvider';
-
-
+import { initializeUser } from './redux/slices/userSlice';
 
 jest.mock("./components/single/Header/Header", () => ({
     __esModule: true,
-  	default: jest.fn(() => (
-   		<div data-testid="Header" />
-  	))
+    default: jest.fn(() => (
+        <div data-testid="Header" />
+    ))
 }))
 jest.mock("./components/single/Footer/Footer", () => ({
     __esModule: true,
@@ -29,35 +28,37 @@ jest.mock("./router/AppRouter", () => ({
 }))
 
 
-describe("App", () => {
+describe("App component", () => {
 
     beforeEach(() => {
         jest.clearAllMocks()
     })
 
-    const useDispatchMock = jest.spyOn(Redux, 'useDispatch')
+    const useDispatchMock = jest.spyOn(CustomReduxHooks, 'useAppDispatch')
+    const fakeDispatch = jest.fn()
 
-  	it("Render app component", () => {
+    useDispatchMock.mockReturnValue(fakeDispatch)
 
-    	render(<AppProvider component={<App />} />)
-
-    	expect(screen.queryByTestId("Header"))
-      		.toBeInTheDocument()
-      	expect(screen.queryByTestId("Footer"))
-      		.toBeInTheDocument()
-      	expect(screen.queryByTestId("AppRouter"))
-      		.toBeInTheDocument()
-
-  	})
-
-    it("Dispatch should have been called", () => {
+    it("Render component", () => {
 
         render(<AppProvider component={<App />} />)
 
-        const fakeDispatch = jest.fn()
-        useDispatchMock.mockReturnValue(fakeDispatch)
+        expect(screen.queryByTestId("Header"))
+            .toBeInTheDocument()
+        expect(screen.queryByTestId("Footer"))
+            .toBeInTheDocument()
+        expect(screen.queryByTestId("AppRouter"))
+            .toBeInTheDocument()
 
-        expect(useDispatchMock).toHaveBeenCalled()
+    })
+
+    it("Dispatch should have been called with action 1 time only", () => {
+
+        render(<AppProvider component={<App />} />)
+
+        expect(fakeDispatch).toHaveBeenCalledWith(initializeUser())
+        expect(fakeDispatch).toHaveBeenCalledTimes(1)
+
     })
 
 })
