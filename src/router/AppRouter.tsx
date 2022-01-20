9749b312dfcea997b10d43de6102d53d2d0c148e
deger4beger <deger.begerrr@gmail.com>
2022-01-20 16:59:55 +0600

    some refactor

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,28 +4,21 @@ import {privateRoutes, publicRoutes, RouteNames} from "../router/router";
 import { useAppSelector } from '../hooks/redux';
 
 const AppRouter = () => {
+
     const isAuth = useAppSelector(state => state.userReducer.isAuth)
 
     return (
         isAuth ? (
                 <Switch>
                     {privateRoutes.map(route =>
-                        <Route path={route.path}
-                            exact={route.exact}
-                            component={route.component}
-                            key={route.path}
-                        />
+                        <Route { ...route } key={route.path} />
                     )}
                     <Redirect to={RouteNames.HOME}/>
                 </Switch>
             ) : (
                 <Switch>
                     {publicRoutes.map(route =>
-                        <Route path={route.path}
-                            exact={route.exact}
-                            component={route.component}
-                            key={route.path}
-                        />
+                        <Route { ...route } key={route.path} />
                     )}
                     <Redirect to={RouteNames.LOGIN}/>
                 </Switch>
