b5281ba32f1e90b51cd5d4349e5a751e54b15a74
deger4beger <deger.begerrr@gmail.com>
2022-01-20 18:18:07 +0600

    PublicMenu test added

---
+++ src/components/single/Header/PublicMenu/PublicMenu.test.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { createMemoryHistory, History } from 'history';
+import userEvent from "@testing-library/user-event";
+import PublicMenu from './PublicMenu';
+
+describe("PiblicMenu component", () => {
+
+	it('Routes /login & /register working', async () => {
+
+		const history: any = createMemoryHistory()
+
+		history.push = jest.fn()
+
+		render(
+		    <Router history={history}>
+		      	<PublicMenu />
+		    </Router>
+		)
+
+		userEvent.click(screen.getByText(/Login/))
+
+		expect(history.push).toHaveBeenCalledWith("/login")
+
+		userEvent.click(screen.getByText(/Sign up/))
+
+		expect(history.push).toHaveBeenCalledWith("/signup")
+
+	})
+})
